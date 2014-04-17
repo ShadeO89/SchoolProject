73817ec6b827fbdd3cbfc5dc9c0afa8607d94236
clawmaster <claes9@yahoo.dk>
2014-04-17 16:44:30 +0200

    Removing faulty terrains
    Unity had lost some data on the terrains, so they had to be made again

--- Assets/scripts/ClickToMove.cs
+++ Assets/scripts/ClickToMove.cs
@@ -6,20 +6,20 @@ public class ClickToMove : MonoBehaviour
 	//int life;
 	//int damage;
 
-	public float speed;
+	public float speed = 10;
 	private Vector3 position;
 	public CharacterController controllerMove;
 
 	// Use this for initialization
 	void Start () 	
 	{
-		position = transform.position;
+		position = this.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.GetMouseButton (0)) {
+		if (Input.GetMouseButton(0)) {
 			locatePosition();		
 		//Locate where the Player clicked terrain
 
@@ -33,23 +33,22 @@ public class ClickToMove : MonoBehaviour
 	void locatePosition()
 	{
 		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-
-		if (Physics.Raycast (ray, out hit, 1000)) 
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)) 
 		{
 			position = new Vector3(hit.point.x , hit.point.y , hit.point.z);
-			//Debug.Log(position);
+			Debug.Log(position);
 		}
 
 	}
 	void movePlayerToPosition(){
-	if(Vector3.Distance(transform.position , position) >1){
+	if(Vector3.Distance(this.transform.position , position) >1){
 		Quaternion newRotation = Quaternion.LookRotation(position-transform.position);
 	
 		newRotation.x = 0f;
 		newRotation.z = 0f;
 
-		transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime);
+		this.transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime);
 		controllerMove.SimpleMove (transform.forward * speed);
 	
 		}
