712fea1ab7577cf0a3a8410e3d30e490cf7dff00
clawmaster <claes9@yahoo.dk>
2014-04-23 19:45:43 +0200

    Control script of mage
    66 % finished control script for mage,
    Missing
    -attack
    - made prefab

---
+++ unity/Assets/scripts/camFollow_claes.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollow_claes : MonoBehaviour {
+
+
+	public Transform Character;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Character.position + new Vector3(-30,30,0);
+	}
+}
---
+++ unity/Assets/scripts/clickMove_claes.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickMove_claes : MonoBehaviour {
+	
+	private Vector3 position;
+	private Vector3 looking;
+
+	void Start () {
+		//forcing him to start at target position
+		this.transform.position = new Vector3(0,3,0);
+	}
+	
+
+	void Update () {
+		get_animplaying();
+	}
+
+	void get_animplaying()
+	{
+		if(this.animation.IsPlaying("idle"))
+		{
+			Debug.Log("idle");
+			set_rotation();
+			set_position();
+
+		}
+		else if(this.animation.IsPlaying("running"))
+		{
+			Debug.Log("running");
+			set_position();
+			moveplayer();
+		}
+		else if(this.animation.IsPlaying("attack"))
+		{
+			Debug.Log("attack");
+		}
+
+	}
+	void set_rotation()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) 
+		{
+			looking = new Vector3(hit.point.x , hit.point.y , hit.point.z);
+			Quaternion rotation = Quaternion.LookRotation (looking - transform.position);
+			rotation *= Quaternion.Euler (0, -90, 0);
+			rotation.x = 0;
+			rotation.z = 0;
+			this.transform.rotation = Quaternion.Lerp (transform.rotation, rotation, Time.deltaTime*5);
+		
+		}
+	}
+	void set_position()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) 
+			{
+				position = new Vector3(hit.point.x , hit.point.y , hit.point.z);
+				Debug.Log(position);
+				this.animation.Play("running");
+				Debug.Log("changed animation");
+			}
+		}
+		else if (this.transform.position == position && !animation.IsPlaying("idle"))
+		{
+			animation.Play("idle");
+			Debug.Log("changed animation");
+		}
+	}
+	void moveplayer()
+	{
+		if (Vector3.Distance (this.transform.position, position) > 1) 
+		{
+
+			Quaternion rotation = Quaternion.LookRotation (position - transform.position);
+			rotation *= Quaternion.Euler (0, -90, 0);
+			rotation.x = 0;
+			rotation.z = 0;
+			this.transform.rotation = Quaternion.Lerp (transform.rotation, rotation, Time.deltaTime*5);
+			this.rigidbody.velocity = transform.right * 10;
+
+		}
+		else
+		{
+			this.transform.position = position;
+		}
+	}
+}
