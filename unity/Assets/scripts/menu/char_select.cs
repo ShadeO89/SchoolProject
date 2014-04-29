ab946770e48f3db732b5e86e7b09f82a4c112eec
clawmaster <claes9@yahoo.dk>
2014-04-29 22:42:26 +0200

    Menu and character selection
    missing
    - character selection should send application onwards to town

---
+++ unity/Assets/scripts/menu/char_select.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class char_select : MonoBehaviour {
+
+	private GameObject choice;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0))
+		{
+			set_char();
+		}
+	
+	}
+	void set_char()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit)) 
+		{
+			if(hit.transform.name == "Terrain")
+			{
+				Debug.Log("i clicked on terrain");
+			}
+			else if(hit.transform.name == "Gunman_blurby")
+			{
+				Debug.Log("i clicked on gunman");
+				PlayerPrefs.SetString("Char","Gunman");
+			}
+			else if(hit.transform.name == "Mage_blurby")
+			{
+				Debug.Log("i clicked on Mage");
+				PlayerPrefs.SetString("Char","Mage");
+			}
+			else if(hit.transform.name == "warrior_blurby")
+			{
+				Debug.Log("i clicked on Warrior");
+				PlayerPrefs.SetString("Char","Warrior");
+			}
+			else
+			{
+				Debug.Log("ive clicked on something");
+				Debug.Log(hit.transform.name);
+			}
+			
+		}
+		Debug.Log(""+ PlayerPrefs.GetString("Char"));
+	}
+}
