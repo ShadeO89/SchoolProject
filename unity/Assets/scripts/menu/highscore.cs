dfe7021ac927c9a4b8064c16e0967e43485f46e6
clawmaster <claes9@yahoo.dk>
2014-04-30 13:30:51 +0200

    upgrade system possible, missing retract gold

--- unity/Assets/scripts/menu/highscore.cs
+++ unity/Assets/scripts/menu/highscore.cs
@@ -15,18 +15,11 @@ public class highscore : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 		firstplace = PlayerPrefs.GetInt("firstplace");
 		secondplace = PlayerPrefs.GetInt("secondplace");
 		thirdplace = PlayerPrefs.GetInt("thirdplace");
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-=======
-		firstplace = PlayerPrefs.GetInt("firstplace");
-		secondplace = PlayerPrefs.GetInt("secondplace");
-		thirdplace = PlayerPrefs.GetInt("thirdplace");
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
+		
 		GetScore();
 		checkScore();
 		screenHeight = Screen.height;
@@ -34,19 +27,11 @@ public class highscore : MonoBehaviour {
 		
 		buttonHeigth = screenHeight * 0.1f;
 		buttonWidth = screenWidth * 0.2f;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 		PlayerPrefs.SetInt("firstplace",firstplace);
 		PlayerPrefs.SetInt("secondplace",secondplace);
 		PlayerPrefs.SetInt("thirdplace",thirdplace);
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-=======
-		PlayerPrefs.SetInt("firstplace",firstplace);
-		PlayerPrefs.SetInt("secondplace",secondplace);
-		PlayerPrefs.SetInt("thirdplace",thirdplace);
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -57,21 +42,10 @@ public class highscore : MonoBehaviour {
 	{
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 			GUI.Label(new Rect (515, 10 * 1, 300, 500), "1.  " + firstplace); 
 			GUI.Label(new Rect (515, 10 * 2, 300, 500), "2.  " + secondplace); 
 			GUI.Label(new Rect (515, 10 * 3, 300, 500), "3.  " + thirdplace); 
-=======
-		GUI.Label(new Rect (515, 10 * 1, 300, 500), "1.  " + firstplace); 
-		GUI.Label(new Rect (515, 10 * 2, 300, 500), "2.  " + secondplace); 
-		GUI.Label(new Rect (515, 10 * 3, 300, 500), "3.  " + thirdplace); 
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-=======
-		GUI.Label(new Rect (515, 10 * 1, 300, 500), "1.  " + firstplace); 
-		GUI.Label(new Rect (515, 10 * 2, 300, 500), "2.  " + secondplace); 
-		GUI.Label(new Rect (515, 10 * 3, 300, 500), "3.  " + thirdplace); 
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
 
 		if (GUI.Button(new Rect((screenWidth - buttonWidth) * 0.5f, screenHeight * 0.2f, buttonWidth, buttonHeigth), "Return to menu")){
 			Application.LoadLevel(0);
@@ -86,19 +60,8 @@ public class highscore : MonoBehaviour {
 	{
 		if(score > firstplace)
 		{
-<<<<<<< HEAD
-<<<<<<< HEAD
-			thirdplace = secondplace; secondplace = firstplace; firstplace = score;
-		}
-		else if(score > secondplace)
-		{
-			thirdplace = secondplace; secondplace = score; 
-		}
-		if(score > thirdplace)
-=======
-=======
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-			thirdplace = secondplace; 
+
+		thirdplace = secondplace; 
 			secondplace = firstplace; 
 			firstplace = score;
 		}
@@ -108,10 +71,7 @@ public class highscore : MonoBehaviour {
 			secondplace = score; 
 		}
 		else if(score > thirdplace)
-<<<<<<< HEAD
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
-=======
->>>>>>> 06d7c9adcd8814c0e5e1c2ea0be3aa142209e0f8
+
 		{
 			thirdplace = score;
 		}
